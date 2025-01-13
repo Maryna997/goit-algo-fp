7eb1b51feaed511bb83db811b3b5281140e132f3
Maryna Zuikova <mzujkova2002@gmail.com>
2025-01-13 14:30:23 +0000

    adding tasks 1 and 2

--- task_01.py
+++ task_01.py
@@ -78,6 +78,60 @@ class LinkedList:
 
 
 
+    # Сортування списку методом вставок
+    def insertion_sort(self):
+        sorted_list = None    # Відсортований список
+        current = self.head
+
+        while current:
+            next_node = current.next
+            # Знайти місце для поточного елементу в відсортованому списку
+            if sorted_list is None or sorted_list.data >= current.data:
+                current.next = sorted_list
+                sorted_list = current 
+            else:
+                sorted_current = sorted_list
+                while sorted_current.next and sorted_current.next.data < current.data:
+                    sorted_current = sorted_current.next
+
+                current.next = sorted_current.next
+                sorted_current.next = current 
+            
+            current = next_node
+
+        self.head = sorted_list
+
+
+    # Об'єднання двох відсортованих списків
+    @staticmethod
+    def merge_sorted_lists(list1: "LinkedList", list2: "LinkedList") -> "LinkedList":
+        merged_list = LinkedList()
+        dummy = Node()  # Допоміжний вузол для початку списку
+        tail = dummy
+
+        current1 = list1.head
+        current2 = list2.head
+
+        # З'єднання двох відсортованих списків
+        while current1 and current2:
+            if current1.data <= current2.data:
+                tail.next = current1
+                current1 = current1.next
+            else:
+                tail.next = current2
+                current2 = current2.next
+            tail = tail.next
+
+        # Якщо є залишки в будь-якому з списків
+        if current1:
+            tail.next = current1
+        elif current2:
+            tail.next = current2
+
+        merged_list.head = dummy.next   # Пропускаємо допоміжний вузол
+        return merged_list
+
+
 # Приклад використання
 llist = LinkedList()
 
@@ -95,3 +149,24 @@ llist.print_list()
 llist.reverse_list()
 print("\nРеверсований зв'язний список:")
 llist.print_list()
+
+# Сортування списку методом вставок
+llist.insertion_sort()
+print("\nВідсортований зв'язний список методом вставок:")
+llist.print_list()
+
+# Створення двох відсортованих списків для об'єднання
+llist1 = LinkedList()
+llist1.insert_at_end(1)
+llist1.insert_at_end(3)
+llist1.insert_at_end(5)
+
+llist2 = LinkedList()
+llist2.insert_at_end(2)
+llist2.insert_at_end(4)
+llist2.insert_at_end(6)
+
+# Об'єднання двох відсортованих списків
+merged_list = LinkedList.merge_sorted_lists(llist1, llist2)
+print("\nОб'єднаний відсортований список з двох відсортованих списків:")
+merged_list.print_list()
